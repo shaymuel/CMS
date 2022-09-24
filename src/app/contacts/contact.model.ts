9f9f6cc974ad93d9a626c8d8673811792c12cd92
shaymuel <97071744+shaymuel@users.noreply.github.com>
2022-09-23 21:59:15 -0600

    initial commit

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,10 @@
+export class Contact {
+    constructor(
+        public id: string, 
+        public name: string,
+        public email: string,
+        public phone: string,
+        public imgUrl: string,
+        public group: string[],){
+    }
+}
